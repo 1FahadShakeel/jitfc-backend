dbd9718235f7f1dd285299ae43a626f761f4f651
Fahad Shakeel <mrfahadshakeel1@gmail.com>
2024-11-05 08:33:52 +0500

    webpack addition

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    target: 'node',
+    mode: 'production',
+    entry: {
+        api: './netlify/functions/api.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'functions-build'),
+        filename: '[name].js',
+        libraryTarget: 'commonjs2'
+    },
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    node: '14'
+                                }
+                            }]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@controllers': path.resolve(__dirname, 'src/controllers'),
+            '@middleware': path.resolve(__dirname, 'src/middleware'),
+            '@utils': path.resolve(__dirname, 'src/utils')
+        }
+    }
+};
